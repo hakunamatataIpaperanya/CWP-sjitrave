4fe4d08f84aecf225b83fe8c6ca0dc3ea026248e
hakunamatatal <sidaporn.jitraveewon@gmail.com>
2026-09-21 18:15:20 +0700

    Add ex01, ex02, and ex03 to cell01

---
+++ cell01/ex01/name.py
@@ -0,0 +1,3 @@
+first_name , last_name= "Sidaporn" , "Jitraveewon"
+whole_name = last_name + " " + first_name
+print(whole_name)
---
+++ cell01/ex02/age.py
@@ -0,0 +1,4 @@
+age = 21
+define_number = 42
+my_age = age + define_number
+print(my_age)
