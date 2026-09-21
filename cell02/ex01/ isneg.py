eebbe99809e87ab651d319213e2ecb7397456b24
hakunamatatal <sidaporn.jitraveewon@gmail.com>
2026-09-21 22:52:21 +0700

    Add cell02 ex01 ex02 ex03

---
+++ cell02/ex01/ isneg.py
@@ -0,0 +1,8 @@
+number = input()
+
+if(int(number) > 0):
+    print("This number is positive.")
+elif(int(number) < 0):
+    print("This number is negative.")
+else:
+    print("This number is both positive and negative.")
---
+++ cell02/ex02/ password.py
@@ -0,0 +1,4 @@
+password = "Python is awesome"
+
+inputPassword = input()
+print("ACCESS GRANTED"if inputPassword == password else "ACCESS DENIED")
